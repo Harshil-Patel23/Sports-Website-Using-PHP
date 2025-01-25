44cd8d3a0f73cdec439a34ce305539a65de78a7a
Harshil Patel <h026patel@gmail.com>
2025-01-25 06:53:34 +0530

    Header file

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
                 <li><a href="cart.php">Cart</a></li>
                 <li><a href="logout.php">Logout</a></li>
             <?php else: ?>
+                
                 <li><a href="login.php">Login</a></li>
                 <li><a href="register.php">Register</a></li>
             <?php endif; ?>
